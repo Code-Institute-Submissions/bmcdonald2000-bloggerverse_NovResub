6f33ba35d4c1796cac88b1850b2ab8fd8ab42e5b
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-29 18:41:42 +0000

    config: edit post url

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, postView, AddPostView
+from .views import HomeView, postView, AddPostView, EditPostView
 
 # Sets the url pattern for each page.
 urlpatterns = [
     path('',  HomeView.as_view(), name="home"),
     path('Posts/<int:pk>/', postView.as_view(), name='details'),
     path('Add_Post/', AddPostView.as_view(), name='posts'),
+    path('Posts/Edit/<int:pk>', EditPostView.as_view(), name='edit'),
 ]
