f3ed8b55f7145c777ebc9f9761f50b81a228886d
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-29 17:55:18 +0000

    config: details urlpattern

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, AddPostView
+from .views import HomeView, postView, AddPostView
 
 # Sets the url pattern for each page.
 urlpatterns = [
     path('',  HomeView.as_view(), name="home"),
+    path('Posts/<int:pk>/', postView.as_view(), name='details'),
     path('Add_Post/', AddPostView.as_view(), name='posts'),
 ]
