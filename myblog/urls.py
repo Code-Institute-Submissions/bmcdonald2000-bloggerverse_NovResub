6fcd6716bea24cf4085ac28bffab14f95ab7d749
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-29 16:33:41 +0000

    config: add post url

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, AddPostView
 
 # Sets the url pattern for each page.
 urlpatterns = [
     path('',  HomeView.as_view(), name="home"),
+    path('Add_Post/', AddPostView.as_view(), name='posts'),
 ]
