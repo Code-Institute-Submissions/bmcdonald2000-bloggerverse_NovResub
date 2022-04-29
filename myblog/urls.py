d00c5ffaff0b7a3ce35f4a8d858f65d24ce50156
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-29 19:28:52 +0000

    config: delete url

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HomeView, postView, AddPostView, EditPostView
+from .views import HomeView, postView, AddPostView
+from .views import EditPostView, DeletePostView
 
 # Sets the url pattern for each page.
 urlpatterns = [
@@ -7,4 +8,6 @@ urlpatterns = [
     path('Posts/<int:pk>/', postView.as_view(), name='details'),
     path('Add_Post/', AddPostView.as_view(), name='posts'),
     path('Posts/Edit/<int:pk>', EditPostView.as_view(), name='edit'),
+    path('Posts/<int:pk>/deletePost/',
+         DeletePostView.as_view(), name='delete'),
 ]
