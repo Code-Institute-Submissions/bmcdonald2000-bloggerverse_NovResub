558830278b8c31968e650cf40f7a9d4cf5d59628
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-29 16:33:55 +0000

    add: post form

---
+++ myblog/forms.py
@@ -0,0 +1,25 @@
+# django imports
+from django import forms
+from .models import Post
+
+
+# creates a form for the posts using a model
+class PostForms(forms.ModelForm):
+
+    # form metadata options
+    class Meta:
+        # using Post model
+        model = Post
+        # fields that will be used for the form
+        fields = ('title', 'category', 'author', 'image', 'body', 'summary')
+
+        # basic controls/styling for the form fields
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control',
+                                            'placeholder':
+                                            'Enter post title'}),
+            'author': forms.TextInput(attrs={'value': '', 'id': 'username',
+                                             'type': 'hidden'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'summary': forms.Textarea(attrs={'class': 'form-control'}),
+        }
