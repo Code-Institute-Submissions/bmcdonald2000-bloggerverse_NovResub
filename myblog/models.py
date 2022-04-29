2bff8182497c036d04de2f3ef9c6e97d5557c482
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-29 17:37:56 +0000

    add: image and date field

--- myblog/models.py
+++ myblog/models.py
@@ -1,21 +1,25 @@
 # django imports, packages and components
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from ckeditor.fields import RichTextField
+from datetime import datetime, date
 
 
 # fields and behaviours for Post model
 class Post(models.Model):
     title = models.CharField(max_length=255)
     # RichTextField gives user all the tool style their post
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
     body = RichTextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     summary = models.CharField(max_length=255)
+    post_date = models.DateField(auto_now_add=True)
 
     # function for admin page to view post title and author
     def __str__(self):
         return self.title + ' | ' + str(self.author)
-    
+
     # function to redirect user to view the post they just made
     def get_absolute_url(self):
         return reverse('details', args=[str(self.id)])
