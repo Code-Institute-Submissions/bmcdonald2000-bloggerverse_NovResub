7789c941788255d1f080895c412ae5d6e5146df9
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-29 17:48:03 +0000

    add: default to datefield

--- myblog/models.py
+++ myblog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from ckeditor.fields import RichTextField
-from datetime import datetime, date
+from datetime import date, datetime
 
 
 # fields and behaviours for Post model
@@ -14,7 +14,8 @@ class Post(models.Model):
     body = RichTextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     summary = models.CharField(max_length=255)
-    post_date = models.DateField(auto_now_add=True)
+    post_date = models.DateField(auto_now_add=True,
+                                 default=datetime.date.today)
 
     # function for admin page to view post title and author
     def __str__(self):
