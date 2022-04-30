92e5a6b62372254c0f4074cf484d87e9ab22d1b4
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-30 00:23:56 +0000

    config: create profile url pattern

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserRegView
+from views import UserRegView, create_profile_view
 
 # URL patters for each page
 urlpatterns = [
     path('Register/', UserRegView.as_view(), name='sign_up'),
+    path('create_profile/', create_profile_view.as_view(),
+         name='create_profile'),
 ]
