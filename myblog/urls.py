db3a269d98a05b16a1ac2f6be48f6354fe1d2dc1
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-30 10:14:17 +0000

    fix: url for postview

--- myblog/urls.py
+++ myblog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('',  HomeView.as_view(), name="home"),
     path('Posts/<int:pk>/', PostView.as_view(), name='details'),
     path('Add_Post/', AddPostView.as_view(), name='posts'),
-    path('Posts/Edit/<int:pk>', EditPostView.as_view(), name='edit'),
+    path('Posts/Edit/<int:pk>/', EditPostView.as_view(), name='edit'),
     path('Posts/<int:pk>/deletePost/',
          DeletePostView.as_view(), name='delete'),
     path('Posts/<int:pk>/add_comment/',
