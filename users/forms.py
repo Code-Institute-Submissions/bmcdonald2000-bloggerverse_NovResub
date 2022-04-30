b7aede3d57866b2156e184ba9dbfcc8ae43c1346
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-04-30 01:16:52 +0000

    add: change password form

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 # django imports
 from myblog.models import UserProfile
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import forms
 
@@ -37,6 +37,7 @@ class RegForm(UserCreationForm):
 
 # creates profile form using django models
 class ProfilePageForm(forms.ModelForm):
+    # form metadata options
     class Meta:
 
         model = UserProfile
@@ -69,3 +70,16 @@ class ProfilePageForm(forms.ModelForm):
                                                'placeholder':
                                                "Linkedin URL"}),
             }
+
+
+# change password form is built using django PasswordChangeForm
+class ChangePasswordForm(PasswordChangeForm):
+
+	    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+	    new_password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+	    new_password2 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+    
+        # form metadata options
+        class Meta:
+            model = User
+            fields = ('old_password', 'new_password1', 'new_password2'),
