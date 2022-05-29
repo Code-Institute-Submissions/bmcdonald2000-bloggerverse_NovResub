98ecdfcb87d78e7196b7b9769a1252400233b628
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-05-29 11:28:10 +0000

    fix: no reverse match error - removed '/'

--- myblog/urls.py
+++ myblog/urls.py
@@ -10,9 +10,9 @@ from .views import LikePostView
 # Sets the url pattern for each page.
 urlpatterns = [
     path('',  HomeView.as_view(), name="home"),
-    path('Posts/<int:pk>/', PostView.as_view(), name='details'),
+    path('Posts/<int:pk>', PostView.as_view(), name='details'),
     path('Add_Post/', AddPostView.as_view(), name='posts'),
-    path('Posts/Edit/<int:pk>/', EditPostView.as_view(), name='edit'),
+    path('Posts/Edit/<int:pk>', EditPostView.as_view(), name='edit'),
     path('Posts/<int:pk>/deletePost/',
          DeletePostView.as_view(), name='delete'),
     path('Posts/<int:pk>/add_comment/',
