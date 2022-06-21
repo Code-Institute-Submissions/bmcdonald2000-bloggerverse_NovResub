adbd392fbfad2fe1d9f0faf343ab1a156dca1f36
Brieanna  McDonald <bmcdonald1406@gmail.com>
2022-06-21 11:52:09 +0000

    add: models to admin

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,7 +1,10 @@
 # django imports
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category, comment, UserProfile
 
 
 # registers my models with the django admin so I can use them.
 admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(comment)
+admin.site.register(UserProfile)
